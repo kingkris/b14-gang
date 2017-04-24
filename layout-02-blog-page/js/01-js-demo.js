eb7402b495a3a2e3eecb800784bcbe5b094aff28
KriS <kris.gowd@gmail.com>
2017-04-24 14:37:04 +0530

    javascript

---
+++ layout-02-blog-page/js/01-js-demo.js
@@ -0,0 +1,15 @@
+//    alert("hey hello,");
+
+
+/*
+  Its
+  a
+  multi
+  line
+  comment
+*/
+
+  userName = prompt('What is your name?');
+
+  message = 'Hey ' + userName + ', Welcome!';
+    document.getElementById('content1').innerHTML = message;
