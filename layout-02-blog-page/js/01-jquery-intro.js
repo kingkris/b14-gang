434d7b12ebb2e2c0bf37005df2a5a5dc72a5f65e
KriS <kris.gowd@gmail.com>
2017-05-02 14:40:12 +0530

    jquery

--- layout-02-blog-page/js/01-jquery-intro.js
+++ layout-02-blog-page/js/01-jquery-intro.js
@@ -1,12 +1,59 @@
-jQuery('.main .lightblue-bg').css({
-  backgroundColor :'red',
-  textTransform :'uppercase'
+$(document).ready(function() {
+  jQuery('.main .lightblue-bg').css({
+    backgroundColor :'red',
+    textTransform :'uppercase'
+  });
+
+
+  jQuery('.side-menu > ul > li > a').click(function(e) {
+    e.preventDefault();
+
+    $('.side-menu > ul > li > a').removeClass('active');
+    $(this).addClass('active');
+    console.log('link clicked was ' + jQuery(this).text());
+    $('.lightblue-bg p').text($(this).text());
+  });
+
+
+
+  $('.btn1').click(function(e) {
+    e.preventDefault();
+/*
+    $('.dabba').css({
+      backgroundColor: 'purple',
+      borderColor: 'green'
+    });
+    $('.dabba').animate({height: '100px', width: '200px'}, 'slow');
+    $('.dabba').slideUp('fast');
+*/
+
+
+    $('.dabba').css({
+      backgroundColor: 'purple',
+      borderColor: 'green'
+    }).animate({height: '100px', width: '200px'}, 'slow').slideUp('fast');
+  });
+
+
+/* Assigning a click function */
+
+$('.title').click(function(event) {
+  /*$(this).text('open');*/
+/*
+  $(this).children('span').toggleClass('close');
+  $(this).next('.side-menu').slideToggle('fast');
+*/
+  $(this).children('span').toggleClass('close').parent().next().slideToggle('fast');
+
 });
 
-jQuery('.side-menu > ul > li > a').click(function(e) {
-  e.preventDefault();
 
-  console.log('link clicked was ' + jQuery(this).text());
 
-  $('.lightblue-bg p').text($(this).text());
+
 });
+
+
+
+
+
+
