53cdf99e734e05f93d173047dea75fae8f53c211
KriS <kris.gowd@gmail.com>
2017-05-12 13:51:25 +0530

    jquery clone

--- layout-02-blog-page/js/02-attaching-events.js
+++ layout-02-blog-page/js/02-attaching-events.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   $('.form-row input').blur(function(event) {
     x = $(this).val();
     if(x){
-      $(this).next('span').text(x).removeClass('error');
+      x = '<strong>' + x + '</strong>';
+      $(this).next('span').html(x).removeClass('error');
+      console.log($(this).parent().text())
     } else{
       $(this).next('span').text("You didn't enter text").addClass('error');
     }
---
+++ layout-02-blog-page/js/03-cloning.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('.add-mobile-btn').click(function(event) {
+    event.preventDefault();
+    lastItem = $(this).prev();
+    lastItem.clone(true,true).removeClass('active').insertAfter(lastItem);
+  });
+
+  $('.rm-btn').click(function(event) {
+    event.preventDefault();
+    console.log("inside remove");
+    $(this).parent().slideUp();
+  });
+
+  $('.df-radio input[type="radio"]').change(function(event) {
+    $('.form-row').removeClass('active');
+    $(this).parentsUntil('form-row').addClass('active');
+  });
+
+});
